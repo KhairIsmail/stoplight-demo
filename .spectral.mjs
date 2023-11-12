f9d38afb137033b85fd289ebaf8b7a1f54bd4c61
mohammad.ismail@sytac.io <mohammad.ismail@sytac.io>
2023-11-12 14:17:49 +0100

    Modified 10 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMzYyNQ/spectral.js?branch_slug=main&token=ee737b77-b613-4fe1-b2fa-1d386549e619";
+export default { extends: ruleset };
---
+++ docs/test.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: nicsyuy8c5ivb
+---
+
+# test
+
+The beginning of an awesome article...
